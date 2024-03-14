a101d9661c896bdef33efd776fa3a0880bec70ac
usanase12 <usanasesandra22@gmail.com>
2024-03-14 11:25:16 +0200

    same change in a script

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function calculateAge() {
-    var birthdate = new Date(document.getElementById('birthdate').value);
-    var today = new Date();
-    var age = today.getFullYear() - birthdate.getFullYear();
-    var monthDiff = today.getMonth() - birthdate.getMonth();
-    
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())) {
+  var birthdate = new Date(document.getElementById('birthdate').value);
+  var today = new Date();
+  var age = today.getFullYear() - birthdate.getFullYear();
+  var monthDiff = today.getMonth() - birthdate.getMonth();
+  
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthdate.getDate())) {
       age--;
-    }
-  
-    document.getElementById('result').innerHTML = "Your age is " + age + " years old.";
   }
-  
+
+  // Convert age to a string and extract the last two characters
+  var ageString = age.toString();
+  var lastTwoDigits = ageString.slice(-2);
+
+  document.getElementById('result').innerHTML = "Your age is " + lastTwoDigits + " years old.";
+}
